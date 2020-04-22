6f7aea78d357d03e6484528cd847bf635c086ba8
Aurora Avery <aurora.avery@yahoo.com>
2020-04-22 11:49:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,16 @@ dwarves = %w[
   Happy
 ]
 
-2.1.0/Enumerable.html#method-i-each_with_index
+# Roll call for the next mining shift!
+#
+# Print out the following list using `puts`:
+# 1. Doc
+# 2. Dopey
+# 3. Bashful
+# etc. for all the dwarves
+#
+# Use the "each_with_index" method:
+# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-each_with_index
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
@@ -16,6 +25,17 @@ def roll_call_dwarves(dwarves)
 end
 
 roll_call_dwarves(dwarves)
+
+# Give the planeteer calls some heart!
+#
+# The summon_captain_planet method should return a new array of values for each
+# of the values in the `planeteer_calls` variable. Each value in the new array
+# should be capitalized and have an exclamation point:
+#
+# earth -> Earth!
+#
+# Use the "map" method (aka the "collection" method):
+# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-map
 
 planeteer_calls = %w[earth wind fire water heart]
 
@@ -26,11 +46,18 @@ end
 
 summon_captain_planet(planeteer_calls)
 
+# The planeteer_calls are supposed to be short, so we want to
+# find out if any of our calls have more than 4 characters.
+#
+# Use the any? method
+# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-any-3F
+
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
 long_planeteer_calls(planeteer_calls)
+
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
