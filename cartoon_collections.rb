ca799e8d75f5a0ad9242559d4c75ecc1ea1bc176
Aurora Avery <aurora.avery@yahoo.com>
2020-04-22 11:51:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,7 @@ dwarves = %w[
 # 2. Dopey
 # 3. Bashful
 # etc. for all the dwarves
-#
-# Use the "each_with_index" method:
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-each_with_index
+
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
