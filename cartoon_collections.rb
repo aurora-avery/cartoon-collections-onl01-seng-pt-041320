8dc88d3ca1f05a2224c1e6c9c212332b103e6484
Aurora Avery <aurora.avery@yahoo.com>
2020-04-22 11:51:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,14 +8,6 @@ dwarves = %w[
   Happy
 ]
 
-# Roll call for the next mining shift!
-#
-# Print out the following list using `puts`:
-# 1. Doc
-# 2. Dopey
-# 3. Bashful
-# etc. for all the dwarves
-
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
